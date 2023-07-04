a8f6cbf74a36aac511484e5a783e022937d6b1db
sru2210 <116788085+sru2210@users.noreply.github.com>
2023-07-04 15:50:47 +0530

    Update SeleniumTest.java

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -19,7 +19,7 @@ public class SeleniumTest {
     @Before
     public void setUp() {
         // Set up ChromeDriver path
-        System.setProperty("webdriver.chrome.driver", "./chromedriver");
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver");
 
         // Create a new ChromeDriver instance
         ChromeOptions options = new ChromeOptions();
